937c829f2266e970c37042c31ad24e7b7382f707
sarmad asad <sarmad@kali.kali>
2025-01-12 15:11:11 +0500

    register service model

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Service
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ('title', 'content')
     list_filter = ('created_at',)
 
+admin.site.register(Service)
 admin.site.register(Blog, BlogAdmin)
