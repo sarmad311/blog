c3deb35fa861a942f8f99b86cf0f305b79f36bdc
sarmad asad <sarmad@kali.kali>
2025-01-17 00:16:28 +0500

    design

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,8 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('blog/', views.blog_list, name='blog_list'),
-    path('blog/create/', views.blog_create, name='blog_create'),
     path('create-service/', views.create_service, name='create_service'),
     path('services/', views.all_services, name='all_services'),
     path('about/', views.about, name='about'),
--- blog_app/views.py
+++ blog_app/views.py
@@ -17,25 +17,6 @@ def admin_check(user):
 def index(request):
     return render(request, "blog_app/index.html")
 
-def blog_list(request):
-    # Fetch all blogs to display
-    blogs = Blog.objects.all().order_by('-created_at')
-    return render(request, 'blog_app/blog_list.html', {'blogs': blogs})
-
-@login_required
-def blog_create(request):
-    if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        comments = request.POST['comments']
-        image = request.FILES['image']
-
-        # Save the blog to the database
-        Blog.objects.create(title=title, content=content, comments=comments, image=image)
-        return redirect('blog_list')  # Redirect to the blog list after creation
-
-    return render(request, 'blog_app/blog_create.html')
-
 @user_passes_test(admin_check)
 def create_service(request):
     if request.method == 'POST':
